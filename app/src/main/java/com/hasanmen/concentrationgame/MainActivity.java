e6405015d64922b21c98cfc91977150be7c60d3a
Hasan MEN <hmen.56@gmail.com>
2017-06-02 01:37:18 +0300

    game logic added

--- app/src/main/java/com/hasanmen/concentrationgame/MainActivity.java
+++ app/src/main/java/com/hasanmen/concentrationgame/MainActivity.java
@@ -1,6 +1,8 @@
 package com.hasanmen.concentrationgame;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
@@ -8,10 +10,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.TextView;
 
 import com.hasanmen.concentrationgame.POJO.PixabayImage;
 
@@ -30,40 +32,66 @@ public class MainActivity extends AppCompatActivity {
     private static final String LOG_KEY = "MainActivity";
 
     private Button btnStart = null;
-    private ArrayList<ImageButton> buttons = new ArrayList<>();
+    private ArrayList<Button> buttons = new ArrayList<>();
     private ArrayList<PixabayImage> pixabayImages = new ArrayList<>();
     private ArrayList<Bitmap> bitmaps = new ArrayList<>();
     private TableLayout tableLayout = null;
     private ProgressBar progressBar = null;
 
     private int clickNum = 0;
-    private ImageButton click2Btn;
-    private ImageButton click1Btn;
+    private Button click2Btn;
+    private Button click1Btn;
+    private Button btn_nextGame;
     private ArrayList<PixabayImage> order = new ArrayList<>();
+
+    private int trueNum;
+    private int falseNum;
+    private int currentGameLevel;
+    TextView tv_score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         btnStart = (Button) findViewById(R.id.btn_start);
         tableLayout = (TableLayout) findViewById(R.id.btnArea);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
         progressBar.setVisibility(View.INVISIBLE);
 
+        currentGameLevel = 4;
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
-                    ImageListDownTask imageListDownTask = new ImageListDownTask();
-                    imageListDownTask.execute(4);
+                    ImageListDownTask imageListDownTask = new ImageListDownTask(currentGameLevel);
+                    imageListDownTask.execute(currentGameLevel);
                 } catch (Exception e) {
-                    Log.d(LOG_KEY, "ImageListDownThread " + e.getMessage());
+                    Log.d(LOG_KEY, "ImageListDownTask " + e.getMessage());
                 }
 
             }
         });
 
+        tv_score = (TextView) findViewById(R.id.tv_score);
+        tv_score.setText(R.string.score_board_init);
+
+        btn_nextGame = (Button) findViewById(R.id.btn_nextGame);
+        btn_nextGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    currentGameLevel += 2;
+                    ImageListDownTask imageListDownTask = new ImageListDownTask(currentGameLevel);
+                    imageListDownTask.execute(currentGameLevel);
+                    btn_nextGame.setVisibility(View.INVISIBLE);
+                    tv_score.setText(R.string.score_board_init);
+                } catch (Exception e) {
+                    Log.d(LOG_KEY, "ImageListDownTask " + e.getMessage());
+                }
+            }
+        });
         // init 4*4 board
         //initBtnArea(4);
 
@@ -79,22 +107,21 @@ public class MainActivity extends AppCompatActivity {
                     TableLayout.LayoutParams.MATCH_PARENT,
                     1.0f
             ));
-            tableLayout.addView(tableRow);
-
             for (int j = 0; j < n; ++j) {
-                ImageButton btn = new ImageButton(MainActivity.this);
+                Button btn = new Button(MainActivity.this);
                 btn.setId(i * n + j);
                 btn.setLayoutParams(new TableRow.LayoutParams(
                         TableRow.LayoutParams.MATCH_PARENT,
                         TableRow.LayoutParams.MATCH_PARENT,
                         1.0f
                 ));
-
+                //btn.setBackground(null);
+                //btn.setPadding(5, 5, 5, 5);
                 btn.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         //Toast.makeText(MainActivity.this,String.valueOf(clickedBtn.getId()),Toast.LENGTH_SHORT).show();
-                        ImageButton clickedBtn = (ImageButton) v;
+                        Button clickedBtn = (Button) v;
                         gameLogic(clickedBtn);
                     }
                 });
@@ -102,37 +129,59 @@ public class MainActivity extends AppCompatActivity {
                 buttons.add(btn);
                 tableRow.addView(btn);
             }
+            tableLayout.addView(tableRow);
         }
     }
 
-    private void gameLogic(ImageButton imgBtn) {
+    private void gameLogic(Button imgBtn) {
 
         if (clickNum == 0) {
-            ++clickNum;
             click1Btn = imgBtn;
-            click1Btn.setImageBitmap(order.get(imgBtn.getId()).getBitmap());
-        } else if(clickNum==1){
+            Bitmap scaledBitmap = Bitmap.createScaledBitmap(order.get(click1Btn.getId()).getBitmap(), imgBtn.getWidth() - 20, imgBtn.getHeight() - 10, true);
+
+            ++clickNum;
+
+            click1Btn.setBackground(new BitmapDrawable(getResources(), scaledBitmap));
+
+            click1Btn.setEnabled(false);
+
+        } else if (clickNum == 1) {
             click2Btn = imgBtn;
             //imgBtn.setImageBitmap(bitmaps.get(imgBtn.getId()));
-            final PixabayImage image1 = order.get(imgBtn.getId());
-            final PixabayImage image2 = order.get(click1Btn.getId());
+            final PixabayImage image1 = order.get(click1Btn.getId());
+            final PixabayImage image2 = order.get(click2Btn.getId());
 
-            imgBtn.setImageBitmap(image1.getBitmap());
+            click2Btn.setBackground(new BitmapDrawable(getResources(), image2.getBitmap()));
 
             if (image1.equals(image2)) { // if images are equal, open two button
                 click1Btn.setEnabled(false);
                 click2Btn.setEnabled(false);
-            }else{
+                ++trueNum;
+            } else {
                 Handler handler = new Handler();
                 handler.postDelayed(new Runnable() {
                     @Override
                     public void run() {
-                        click1Btn.setImageBitmap(null);
-                        click2Btn.setImageBitmap(null);
+                        Bitmap scaledQuestion = BitmapFactory.decodeResource(getResources(), R.drawable.question_mark);
+                        scaledQuestion = Bitmap.createScaledBitmap(scaledQuestion, click1Btn.getWidth(), click1Btn.getHeight(), true);
+                        BitmapDrawable bitmapDrawable = new BitmapDrawable(getResources(), scaledQuestion);
+
+                        click1Btn.setBackground(bitmapDrawable);
+                        click1Btn.setEnabled(true);
+                        click2Btn.setBackground(bitmapDrawable);
+                        click2Btn.setEnabled(true);
                     }
-                },300);
+                }, 300);
+                ++falseNum;
             }
             --clickNum;
+        }
+        tv_score.setText("SCORE -> True:" + trueNum + " False:" + falseNum);
+
+        if (trueNum == currentGameLevel * 2 && falseNum < 20) {
+            btn_nextGame.setVisibility(View.VISIBLE);
+            trueNum = 0;
+            falseNum = 0;
         }
 
     }
@@ -148,8 +197,9 @@ public class MainActivity extends AppCompatActivity {
 
         private int imageNumber; // default different image number
 
-        public ImageListDownTask() {
+        public ImageListDownTask(int imageNumber) {
             super();
+            this.imageNumber = imageNumber;
         }
 
         @Override
@@ -157,6 +207,7 @@ public class MainActivity extends AppCompatActivity {
             super.onPreExecute();
             progressBar.setVisibility(View.VISIBLE);
             progressBar.setProgress(0);
+            initBtnArea(imageNumber);
         }
 
         @Override
@@ -164,10 +215,12 @@ public class MainActivity extends AppCompatActivity {
             super.onPostExecute(imageNumber);
             progressBar.setVisibility(View.GONE);
 
-            initBtnArea(imageNumber);
-           /* for (int i = 0; i < imageNumber * imageNumber; ++i) {
-                buttons.get(i).setImageBitmap(order.get(i).getBitmap());
-            }*/
+            Bitmap scaledQuestion = BitmapFactory.decodeResource(getResources(), R.drawable.question_mark);
+            scaledQuestion = Bitmap.createScaledBitmap(scaledQuestion, buttons.get(0).getWidth() - 10, buttons.get(0).getHeight() - 10, true);
+            BitmapDrawable bitmapDrawable = new BitmapDrawable(getResources(), scaledQuestion);
+            for (int i = 0; i < buttons.size(); ++i) {
+                buttons.get(i).setBackground(bitmapDrawable);
+            }
         }
 
         @Override
@@ -224,7 +277,7 @@ public class MainActivity extends AppCompatActivity {
 
 
                 for (int i = 0; i < imageNumber * 2; ++i) {
-                    ImageDownThread imageDownThread = new ImageDownThread(pixabayImages.get(i / 2),order);
+                    ImageDownThread imageDownThread = new ImageDownThread(pixabayImages.get(i / 2), order);
                     Log.d(LOG_KEY, pixabayImages.get(i / 2).toString());
                     imageDownThreads.add(imageDownThread);
                     imageDownThread.start();
