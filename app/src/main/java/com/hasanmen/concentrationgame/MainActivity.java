88252a821fdd255558637a70fe6223b62a9939cc
Hasan MEN <hmen.56@gmail.com>
2017-06-03 14:52:26 +0300

    added game icon and change system to pass next levels

--- app/src/main/java/com/hasanmen/concentrationgame/MainActivity.java
+++ app/src/main/java/com/hasanmen/concentrationgame/MainActivity.java
@@ -31,11 +31,13 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_KEY = "MainActivity";
     private final static String REQUEST = "https://pixabay.com/api/?key=5429432-e78aeb946ac4de95966734b5c";
+    private final int IMAGE_CROP_SIZE = 30;
 
     private ArrayList<Button> buttons = new ArrayList<>();
     private ArrayList<PixabayImage> pixabayImages = new ArrayList<>();
     private TableLayout tableLayout = null;
     private TextView tv_score;
+    private TextView tv_change;
     private ArrayList<PixabayImage> randomImages = new ArrayList<>();
 
     private Button btnStart = null;
@@ -46,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
     private int falseNum; // number of fail guess
     private int currentGameLevel;
     private int clickNum; // number of button clicks
+    private int changeToNextGame;
 
 
     @Override
@@ -60,120 +63,136 @@ public class MainActivity extends AppCompatActivity {
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try {
-
-                    pixabayImages.clear();
-                    buttons.clear();
-                    randomImages.clear();
-                    Toast.makeText(MainActivity.this,"Downloading Images",Toast.LENGTH_LONG).show();
-
-                    Thread imageListDownThread = new Thread(new Runnable() {
-                        @Override
-                        public void run() {
-                            final String LOG_THREAD1_KEY = "GeneralImageDownThread";
-                            StringBuilder timeSB = new StringBuilder();
-                            long execTime = System.currentTimeMillis();
-                            timeSB.append("Start:").append(new SimpleDateFormat("HH:mm:ss:SSS ").format(Calendar.getInstance().getTime()));
-                            URL url = null;
-                            HttpURLConnection urlConnection = null;
-                            ArrayList<ImageDownThread> imageDownThreads = new ArrayList<>(); // these thread will parallel download images
-
-                            try {
-                                int imageNumber;
-                                StringBuilder requestSB = new StringBuilder(REQUEST);
-                                imageNumber = currentGameLevel * currentGameLevel / 2;
-                                // prepare request
-                                requestSB.append("&per_page=" + imageNumber);
-                                requestSB.append("&q=flowers");
-                                requestSB.append("&order=latest");
-
-                                url = new URL(requestSB.toString());
-                                urlConnection = (HttpURLConnection) url.openConnection();
-
-                                Log.d(LOG_THREAD1_KEY, "Request url:" + requestSB.toString());
-
-                                StringBuilder jsonMsg = new StringBuilder();
-
-                                BufferedReader bfr = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-
-                                String line;
-                                while ((line = bfr.readLine()) != null) {
-                                    jsonMsg.append(line);
-                                }
-                                //Log.d(LOG_KEY, "Response JSON:"+jsonMsg.toString());
-
-                                // parse json string and get image list
-                                pixabayImages = parsePixaJSON(jsonMsg.toString());
-
-                                for (int i = 0; i < imageNumber * 2; ++i) {
-                                    ImageDownThread imageDownThread = new ImageDownThread(pixabayImages.get(i / 2), randomImages);
-                                    imageDownThreads.add(imageDownThread);
-                                    imageDownThread.start();
-                                    Log.d(LOG_THREAD1_KEY, pixabayImages.get(i / 2).toString());
-                                }
-                                Log.d(LOG_THREAD1_KEY, "Download :" + imageNumber + " image address");
-
-                                for (int i = 0; i < imageNumber * 2; ++i) {
-                                    imageDownThreads.get(i).join();
-                                    //Log.d(LOG_THREAD1_KEY, "Thread " + i + " joined");
-                                }
-
-
-                            } catch (Exception e) {
-                                Log.e(LOG_THREAD1_KEY, e.getMessage());
-                                e.printStackTrace();
-                                runOnUiThread(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        Toast.makeText(MainActivity.this, "Downloading Images failed!", Toast.LENGTH_LONG).show();
-                                    }
-                                });
-                            } finally {
-                                // close url connection
-                                if (urlConnection != null)
-                                    urlConnection.disconnect();
-
-                                execTime = System.currentTimeMillis()-execTime;
-                                timeSB.append("-- End:").append(new SimpleDateFormat("HH:mm:ss:SSS ").format(Calendar.getInstance().getTime()));
-                                timeSB.append("-- Execution Time(ms):").append(String.valueOf(execTime));
-                                Log.d(LOG_THREAD1_KEY, timeSB.toString());
-                            }
-
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    setButtonBackgrounds();
-                                }
-                            });
-                        }
-                    });
-
-                    initBtnArea(currentGameLevel);
-                    imageListDownThread.start();
-                    tv_score.setText(R.string.score_board_init);
-                } catch (Exception e) {
-                    Log.d("ImageListDownThread", e.getMessage());
-                } finally {
-                }
-
-
+                btnStart.setEnabled(false);
+                startGame();
             }
         });
 
+
         tv_score = (TextView) findViewById(R.id.tv_score);
         tv_score.setText(R.string.score_board_init);
+
+        tv_change = (TextView) findViewById(R.id.tv_change);
+
+    }
+
+    private void startGame() {
+
+        try {
+            pixabayImages.clear();
+            buttons.clear();
+            randomImages.clear();
+
+            Toast.makeText(MainActivity.this, "Downloading Images", Toast.LENGTH_SHORT).show();
+
+            Thread imageListDownThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    final String LOG_THREAD1_KEY = "GeneralImageDownThread";
+                    StringBuilder timeSB = new StringBuilder();
+                    long execTime = System.currentTimeMillis();
+                    timeSB.append("Start:").append(new SimpleDateFormat("HH:mm:ss:SSS ").format(Calendar.getInstance().getTime()));
+                    URL url = null;
+                    HttpURLConnection urlConnection = null;
+                    ArrayList<ImageDownThread> imageDownThreads = new ArrayList<>(); // these thread will parallel download images
+
+                    try {
+                        int imageNumber;
+                        StringBuilder requestSB = new StringBuilder(REQUEST);
+                        imageNumber = currentGameLevel * currentGameLevel / 2;
+                        // prepare request
+                        requestSB.append("&per_page=" + imageNumber);
+                        requestSB.append("&q=flowers");
+                        requestSB.append("&order=latest");
+
+                        url = new URL(requestSB.toString());
+                        urlConnection = (HttpURLConnection) url.openConnection();
+
+                        Log.d(LOG_THREAD1_KEY, "Request url:" + requestSB.toString());
+
+                        StringBuilder jsonMsg = new StringBuilder();
+
+                        BufferedReader bfr = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+
+                        String line;
+                        while ((line = bfr.readLine()) != null) {
+                            jsonMsg.append(line);
+                        }
+                        //Log.d(LOG_KEY, "Response JSON:"+jsonMsg.toString());
+
+                        // parse json string and get image list
+                        pixabayImages = parsePixaJSON(jsonMsg.toString());
+
+                        for (int i = 0; i < imageNumber * 2; ++i) {
+                            ImageDownThread imageDownThread = new ImageDownThread(pixabayImages.get(i / 2), randomImages);
+                            imageDownThreads.add(imageDownThread);
+                            imageDownThread.start();
+                            Log.d(LOG_THREAD1_KEY, pixabayImages.get(i / 2).toString());
+                        }
+                        Log.d(LOG_THREAD1_KEY, "Download :" + imageNumber + " image address");
+
+                        for (int i = 0; i < imageNumber * 2; ++i) {
+                            imageDownThreads.get(i).join();
+                            //Log.d(LOG_THREAD1_KEY, "Thread " + i + " joined");
+                        }
+                    } catch (Exception e) {
+                        Log.e(LOG_THREAD1_KEY, e.getMessage());
+                        e.printStackTrace();
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(MainActivity.this, "Downloading Images failed!", Toast.LENGTH_LONG).show();
+                            }
+                        });
+                    } finally {
+                        // close url connection
+                        if (urlConnection != null)
+                            urlConnection.disconnect();
+
+                        execTime = System.currentTimeMillis() - execTime;
+                        timeSB.append("-- End:").append(new SimpleDateFormat("HH:mm:ss:SSS ").format(Calendar.getInstance().getTime()));
+                        timeSB.append("-- Execution Time(ms):").append(String.valueOf(execTime));
+                        Log.d(LOG_THREAD1_KEY, timeSB.toString());
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                btnStart.setText("RESTART");
+
+                                btnStart.setEnabled(true);
+                            }
+                        });
+                    }
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            setButtonBackgrounds();
+                        }
+                    });
+                }
+            });
+
+            initBtnArea(currentGameLevel);
+            imageListDownThread.start();
+            tv_score.setText(R.string.score_board_init);
+            changeToNextGame = currentGameLevel * 10;
+            tv_change.setText("Change:" + changeToNextGame);
+        } catch (Exception e) {
+            Log.d("ImageListDownThread", e.getMessage());
+        } finally {
+        }
+
     }
 
     private void setButtonBackgrounds() {
         Bitmap scaledQuestion = BitmapFactory.decodeResource(getResources(), R.drawable.question_mark);
-        scaledQuestion = Bitmap.createScaledBitmap(scaledQuestion, buttons.get(0).getWidth() - 30, buttons.get(0).getHeight() - 30, true);
+        scaledQuestion = Bitmap.createScaledBitmap(scaledQuestion, buttons.get(0).getWidth() - IMAGE_CROP_SIZE, buttons.get(0).getHeight() - IMAGE_CROP_SIZE, true);
         BitmapDrawable bitmapDrawable = new BitmapDrawable(getResources(), scaledQuestion);
         for (int i = 0; i < buttons.size(); ++i) {
             buttons.get(i).setBackground(bitmapDrawable);
             buttons.get(i).setEnabled(true);
         }
-
-        Toast.makeText(MainActivity.this,"Images Downloaded.",Toast.LENGTH_LONG).show();
+        Toast.makeText(MainActivity.this, "Images Downloaded.", Toast.LENGTH_SHORT).show();
     }
 
     private void initBtnArea(int n) {
@@ -216,7 +235,7 @@ public class MainActivity extends AppCompatActivity {
 
         if (clickNum == 0) {
             click1Btn = imgBtn;
-            Bitmap scaledBitmap = Bitmap.createScaledBitmap(randomImages.get(click1Btn.getId()).getBitmap(), imgBtn.getWidth() - 30, imgBtn.getHeight() - 30, true);
+            Bitmap scaledBitmap = Bitmap.createScaledBitmap(randomImages.get(click1Btn.getId()).getBitmap(), imgBtn.getWidth() - IMAGE_CROP_SIZE, imgBtn.getHeight() - IMAGE_CROP_SIZE, true);
 
             ++clickNum;
 
@@ -242,7 +261,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         Bitmap scaledQuestion = BitmapFactory.decodeResource(getResources(), R.drawable.question_mark);
-                        scaledQuestion = Bitmap.createScaledBitmap(scaledQuestion, click1Btn.getWidth(), click1Btn.getHeight(), true);
+                        scaledQuestion = Bitmap.createScaledBitmap(scaledQuestion, click1Btn.getWidth() - IMAGE_CROP_SIZE, click1Btn.getHeight() - IMAGE_CROP_SIZE, true);
                         BitmapDrawable bitmapDrawable = new BitmapDrawable(getResources(), scaledQuestion);
 
                         click1Btn.setBackground(bitmapDrawable);
@@ -252,15 +271,29 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }, 300);
                 ++falseNum;
+                --changeToNextGame;
+                tv_change.setText("Change:" + changeToNextGame);
             }
             --clickNum;
+
+
         }
         tv_score.setText("SCORE -> True:" + trueNum + " False:" + falseNum);
 
-        if (trueNum == currentGameLevel * 2 && falseNum < 20) {
-            //btn_nextGame.setVisibility(View.VISIBLE);
+        if (trueNum == (currentGameLevel * 2) && (falseNum < (currentGameLevel * 10))) {
             trueNum = 0;
             falseNum = 0;
+            clickNum = 0;
+            currentGameLevel += 2;
+            startGame();
+        } else if (trueNum == currentGameLevel * 2) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(MainActivity.this, "You Failed! Try Again!", Toast.LENGTH_LONG).show();
+                }
+            });
+
         }
     }
 
